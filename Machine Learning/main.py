5b9cfb1d1a1eafe044026dac75d165a3e808289f
Neuxbane <65763323+Neuxbane@users.noreply.github.com>
2023-01-16 15:10:26 +0000

    a

--- Machine Learning/main.py
+++ Machine Learning/main.py
@@ -6,7 +6,7 @@ import time
 path_to_file = tf.keras.utils.get_file(
     'shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
-text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+text = open('./text.txt', 'rb').read().decode(encoding='utf-8')
 
 vocab = sorted(set(text))
 
